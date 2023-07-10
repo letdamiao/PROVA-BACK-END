c377ff0b8984af11b8875f8f5bf31c73144bc629
Letícia Damião <108810404+letdamiao@users.noreply.github.com>
2023-07-10 12:34:30 -0300

    Add files via upload

---
+++ Letícia Damião/PROVA/index.js
@@ -0,0 +1,64 @@
+// ATIVIDADE 01
+
+const http = require('http')
+// const PORT = 5000
+// const server = http.createServer((req, res) =>{
+  //  if(''){
+        // res.setHeader('Content-type', 'aplication/json');
+        // res.writeHeader('<h1>sistema operacional do servidor</h1>')
+        // res.write('<p></p>')
+        // res.end()
+  //  }else if{
+
+  //  }
+
+
+//  })
+//  server.listen(PORT, ()=>{
+//     console.log(`SERVIDOR DA PORTA 5000`)
+
+//  })
+// ATIVIDADE 02
+// const http = require('http')
+// const url = require('url')
+// const address = 'https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf'
+// const parseUrl = novo.URL(address)
+// console.log(parseUrl.pathname)
+// Server.listen(http, () => {
+//     console.log(`servidor:`)
+// })
+
+// ATIVIDADE 04 
+
+
+// const fs = require('fs')
+
+// const arq01 = 'arquivo.txt'
+// const arq02 = 'novo.txt'
+// fs.rename(arq01,arq02, function(err){
+//      if(err){
+//       console.log(err)
+//     return
+//     }
+//      console.log(`O arquivo ${arq01} foi renomeado para ${arq02}`)
+// })
+
+
+// ATIVIDADE 05
+
+// const os = require('os')
+// console.log(os.cpus())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
